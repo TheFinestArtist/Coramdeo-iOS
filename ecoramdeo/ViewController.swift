39916e265ef863c5ad220fcc52838f69fd4dc432
Wonhee David Lee <wonheedavid@wonheedavid-macbookpro2.roam.corp.google.com>
2017-06-26 13:57:53 +0900

    Update apps

--- ecoramdeo/ViewController.swift
+++ ecoramdeo/ViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.delegate = self;
         webView.loadRequest(URLRequest(url: URL(string: "http://ecoramdeo.com/")!))
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        self.view.backgroundColor = UIColor(red:0.20, green:0.24, blue:0.27, alpha:1.0)
     }
 }
 
