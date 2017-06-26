8602a6788024384b59b994d155402f8ebb722908
Wonhee David Lee <wonheedavid@dhcp-172-30-116-48.seo.corp.google.com>
2017-06-26 11:07:20 +0900

    Initial Commit

---
+++ ecoramdeo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ecoramdeo
+//
+//  Created by Wonhee David Lee on 6/26/17.
+//  Copyright © 2017 Coramdeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
