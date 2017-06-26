555a1119a9add49f6affd6b0825616387bad2410
Wonhee David Lee <wonheedavid@wonheedavid-macbookpro2.roam.corp.google.com>
2017-06-26 11:38:35 +0900

    First commit

--- ecoramdeo/ViewController.swift
+++ ecoramdeo/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.loadRequest(URLRequest(url: URL(string: "http://ecoramdeo.com/")!))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
